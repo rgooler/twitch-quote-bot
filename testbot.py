c79970d95e9d83b8e5d1af92609039088a10aa4b
Ryan Gooler <cheetahmorph@gmail.com>
2014-07-06 02:50:41 -0700

    Tweaking configs

--- testbot.py
+++ testbot.py
@@ -23,11 +23,11 @@ The known commands are:
 import irc.bot
 import irc.strings
 import settings
-from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+from irc.client import ip_quad_to_numstr
 
 
 class TestBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, channel, nickname, server, port=6667):
+    def __init__(self, channel, nickname, server, password, port=6667):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname,
                                             nickname)
         self.channel = channel
@@ -38,29 +38,11 @@ class TestBot(irc.bot.SingleServerIRCBot):
     def on_welcome(self, c, e):
         c.join(self.channel)
 
-    def on_privmsg(self, c, e):
-        self.do_command(e, e.arguments[0])
-
     def on_pubmsg(self, c, e):
-        a = e.arguments[0].split(":", 1)
-        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname)():
-            self.do_command(e, a[1].strip())
-        return
-
-    def on_dccmsg(self, c, e):
-        c.privmsg("You said: " + e.arguments[0])
-
-    def on_dccchat(self, c, e):
-        if len(e.arguments) != 2:
-            return
-        args = e.arguments[1].split()
-        if len(args) == 4:
-            try:
-                address = ip_numstr_to_quad(args[2])
-                port = int(args[3])
-            except ValueError:
-                return
-            self.dcc_connect(address, port)
+        if e.startswith('!'):
+            # do stuff
+            # self.do_command(e, a[1].strip())
+            pass
 
     def do_command(self, e, cmd):
         nick = e.source.nick
@@ -93,6 +75,6 @@ class TestBot(irc.bot.SingleServerIRCBot):
 
 
 if __name__ == "__main__":
-    bot = TestBot(settings.CHANNEL, settings.NICKNAME, settings.SERVER,
-                  settings.PORT)
+    s = settings
+    bot = TestBot(s.CHANNEL, s.USER, s.HOST, s.PASSWORD, s.PORT)
     bot.start()
