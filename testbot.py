8a0cb0b5614b58f6975176b68b03e9b59d8a6acf
Ryan Gooler <cheetahmorph@gmail.com>
2014-07-06 19:40:45 -0700

    Some more filtering

--- testbot.py
+++ testbot.py
@@ -1,25 +1,4 @@
 #!virtualenv/bin/python
-"""A simple example bot.
-
-This is an example bot that uses the SingleServerIRCBot class from
-irc.bot.  The bot enters a channel and listens for commands in
-private messages and channel traffic.  Commands in channel messages
-are given by prefixing the text by the bot name followed by a colon.
-It also responds to DCC CHAT invitations and echos data sent in such
-sessions.
-
-The known commands are:
-
-    stats -- Prints some channel information.
-
-    disconnect -- Disconnect the bot.  The bot will try to reconnect
-                  after 60 seconds.
-
-    die -- Let the bot cease to exist.
-
-    dcc -- Let the bot invite you to a DCC CHAT connection.
-"""
-
 import irc.bot
 import irc.strings
 import settings
@@ -38,40 +17,29 @@ class TestBot(irc.bot.SingleServerIRCBot):
         c.join(self.channel)
 
     def on_join(self, c, e):
-        s = 'CREATE TABLE IF NOT EXISTS %s_quotes '
-        s += '(id INTEGER PRIMARY KEY AUTOINCREMENT, quote TEXT)'
-        self.cursor.execute(s % e.target[1:])
+        self.create_dbs(e.target)
 
     def on_pubmsg(self, c, e):
         recv = e.arguments[0]
+        cmd = recv.split(' ')[0].lower()
         if not recv.startswith('!'):
             return
-        if recv.startswith('!addquote'):
+        if cmd == '!addquote' and self.check_perms(c, e, cmd):
             self.cmd_addquote(c, e)
-            # if nick in self.channels[e.target].operdict:
-            #    self.cmd_addquote(e)
-            #    return
-            # else:
-            #     print 'not op'
-            #    self.do_command(e, '!addquote')
-            #    return
-        if recv.startswith('!quote'):
-            self.cmd_sayquote(c, e)
+        if recv.startswith('!quote') and self.check_perms(c, e, cmd):
+            self.cmd_quote(c, e)
 
     def cmd_addquote(self, c, e):
-        print 'cmd_addquote()'
-        quote = e.arguments[0]
+        quote = str(e.arguments[0][len('!addquote'):].strip())
         if quote is None:
             return
-        # quote = str(e.arguments[0][len('!addquote'):].strip())
         sql = "INSERT INTO %s_quotes (quote) VALUES (?);" % e.target[1:]
         self.cursor.execute(sql, (quote,))
         self.db.commit()
 
-    def cmd_sayquote(self, c, e, recursions=0):
+    def cmd_quote(self, c, e, recursions=0):
         if recursions >= 3:
             return
-        print 'cmd_sayquote()'
         # output random quote
         sql = 'SELECT MAX(id) FROM %s_quotes' % e.target[1:]
         self.cursor.execute(sql)
@@ -81,11 +49,41 @@ class TestBot(irc.bot.SingleServerIRCBot):
         self.cursor.execute(sql, m)
         try:
             (quote,) = self.cursor.fetchone()
-            print quote
             c.privmsg(e.target, quote)
         except:
             # Sometimes, this returns a none. If so, try again
-            self.cmd_sayquote(c, e, recursions + 1)
+            self.cmd_quote(c, e, recursions + 1)
+
+    def create_dbs(self, channel):
+        channel = channel.strip('#')
+        s = 'CREATE TABLE IF NOT EXISTS %s_quotes '
+        s += '(id INTEGER PRIMARY KEY AUTOINCREMENT, quote TEXT)'
+        self.cursor.execute(s % channel)
+        s = 'CREATE TABLE IF NOT EXISTS %s_regulars '
+        s += '(id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT)'
+        self.cursor.execute(s % channel)
+        self.db.commit()
+
+    def check_perms(self, c, e, cmd):
+        channel = e.target[:1]
+        nick = e.source.nick
+        if nick in self.channels[e.target].operdict:
+            return True
+        if cmd == '!addquote':
+            return self.user_is_regular(channel, nick)
+        else:
+            return True
+
+    def user_is_regular(self, channel, nick):
+        sql = 'SELECT * FROM %s_regulars WHERE user=?' % channel
+        self.cursor.execute(sql, (nick, ))
+        try:
+            if self.cursor.fetchone() is None:
+                return False
+            return True
+        except:
+            return False
+        return False
 
 if __name__ == "__main__":
     s = settings
